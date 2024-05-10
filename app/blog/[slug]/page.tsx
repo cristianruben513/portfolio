82ffce58b288419de552b4303d7453f248519510
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2024-05-10 16:39:47 -0600

    disable edge runtime for netlify deploy

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -6,8 +6,56 @@ import Tags from "@/app/components/Tags";
 import Avatar from "@/app/components/ui/Avatar";
 import Link from "@/app/components/ui/Link";
 import Me from "@/public/avatar.webp";
+import type { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+
+type Props = {
+  params: {
+    slug: string;
+    id: string;
+  };
+  searchParams: { [key: string]: string | string[] | undefined };
+};
+
+export async function generateMetadata(
+  { params }: Props,
+): Promise<Metadata> {
+
+  const post = allPosts.find((post) => post.slug === params.slug);
+
+  if (!post) {
+    notFound();
+  }
+
+  const {
+    title,
+    publishedAt: publishedTime,
+    summary: description,
+    image,
+    slug,
+  } = post;
+
+  const ogImage = image
+    ? `https://cristianfigueroa.dev/${image}`
+    : `https://cristianfigueroa.dev/api/og?title=${title}`;
+
+  const metadata: Metadata = {
+    metadataBase: new URL("https://cristianfigueroa.dev"),
+    title: `${title} | Cristian Ruben`,
+    description,
+    openGraph: {
+      title: `${title} | Cristian Ruben`,
+      description,
+      type: "article",
+      publishedTime,
+      url: `https://cristianfigueroa.dev/blog/${slug}`,
+      images: [{ url: ogImage, alt: title }],
+    },
+  };
+
+  return metadata;
+}
 
 export default async function Blog({ params }: { params: any }) {
   const post = allPosts.find((post) => post.slug === params.slug);
