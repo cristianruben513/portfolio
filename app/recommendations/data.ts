78c46964f55747e543b9cb46910cb92793e38287
Cristian Ruben <cristian.figueroa.crfe@gmail.com>
2024-05-11 01:13:13 -0600

    update recommendations

--- app/recommendations/data.ts
+++ app/recommendations/data.ts
@@ -17,13 +17,6 @@ export const gear = [
     image: "https://m.media-amazon.com/images/I/61xKiCADfpL._AC_SL1500_.jpg",
   },
   {
-    name: "Apple Studio Display",
-    category: "Home Desk Setup",
-    description: "A 3440 x 1440 IPS Display, great for multitasking.",
-    image:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/studio-display-gallery-1-202203?wid=640&hei=528&fmt=p-jpg&qlt=95&.v=1675709041796",
-  },
-  {
     name: "Superhuman Mail",
     category: "Apps",
     image: Superhuman,
@@ -31,23 +24,9 @@ export const gear = [
   },
   
   {
-    name: "Brevite Backpack",
-    category: "Everyday Carry",
-    image: "https://m.media-amazon.com/images/I/61kuICMElAL._AC_SL1500_.jpg",
-    description: "Compact Camera Backpack - A Minimalist & Travel-friendly",
-  },
-  {
-    name: "MacBook Pro M1 Pro 16-inch",
+    name: "MacBook Air M1",
     category: "Everyday Carry",
     image: "https://m.media-amazon.com/images/I/61bwiPRcv2L._AC_SL1500_.jpg",
     description: "Daily driver. Sleek, great display, and performant.",
-  },
-  {
-    name: "Apple AirPods Pro",
-    category: "Everyday Carry",
-    image:
-      "https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/MQD83_AV5?wid=572&hei=572&fmt=jpeg&qlt=95&.v=1660803961739",
-    description:
-      "Wireless Earbuds, Up to 2X More Active Noise Cancelling, Adaptive Transparency.",
   },
 ];
