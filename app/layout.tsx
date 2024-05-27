98aa3dc8a1e250432341c5376c9b45d0d3ca8b63
Cristian Ruben <figueroac513@gmail.com>
2024-05-26 23:23:23 -0600

    add analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
+import { Analytics } from "@vercel/analytics/react"
+
 // Since we have a root `not-found.tsx` page, a layout file
 // is required, even if it's just passing children through.
 
 export default function RootLayout({children}: {children: React.ReactNode}) {
-  return children;
+  <>
+    <Analytics />
+    {children}
+  </>
 }
